04407d2b1c3188038d3f610fd1d0b9aea4cd2dc9
YASHI0211 <yashigupta0211@gmail.com>
2026-02-19 11:18:33 +0530

    Time: 0 ms (100%), Space: 110.9 MB (77.94%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,20 +1,41 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int totalEdges = edges.length;
-        HashMap<Integer, Integer> freqMap =  new HashMap<Integer, Integer>();
-        for (int i=0; i< edges.length; i++){
-            int v1= edges[i][0];
-            int v2 =edges[i][1];
-            freqMap.put(v1, freqMap.getOrDefault(v1,0)+1);
-            freqMap.put(v2, freqMap.getOrDefault(v2,0)+1);
-            if (freqMap.get(v1)==totalEdges){
-                return v1;
-            }
-            if (freqMap.get(v2)==totalEdges){
-                return v2;
+
+            int pair1[] = edges[0];
+            int pair2[] = edges[1];
+            if (pair1[0]==pair2[0]|| pair1[0]==pair2[1]){
+                return pair1[0];
             }
             
-        }
-        return -1;
+            return pair1[1];
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        // int totalEdges = edges.length;
+        // HashMap<Integer, Integer> freqMap =  new HashMap<Integer, Integer>();
+        // for (int i=0; i< edges.length; i++){
+        //     int v1= edges[i][0];
+        //     int v2 =edges[i][1];
+        //     freqMap.put(v1, freqMap.getOrDefault(v1,0)+1);
+        //     freqMap.put(v2, freqMap.getOrDefault(v2,0)+1);
+        //     if (freqMap.get(v1)==totalEdges){
+        //         return v1;
+        //     }
+        //     if (freqMap.get(v2)==totalEdges){
+        //         return v2;
+        //     }
+            
+        // }
+        // return -1;
     }
 }
