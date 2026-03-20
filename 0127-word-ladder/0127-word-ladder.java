41ba2c163d8616dbab2ad085324b1e6864af8506
YASHI0211 <yashigupta0211@gmail.com>
2026-03-20 18:37:58 +0530

    Time: 193 ms (20.59%), Space: 48.3 MB (52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,31 +1,51 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>(wordList);
-        if (!set.contains(endWord)) return 0;
-        Queue<String> q = new LinkedList<>();
-        q.add(beginWord);
-        int steps = 1;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                String curr = q.poll();
-                if (curr.equals(endWord)) return steps;
-                char[] chars = curr.toCharArray();
-                for (int j = 0; j < chars.length; j++) {
-                    char temp = chars[j];
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        chars[j] = c;
-                        String next = new String(chars);
-                        if (set.contains(next)) {
-                            q.add(next);
-                            set.remove(next);
-                        }
-                    }
-                    chars[j] = temp;
-                }
+    public List<String> getNeighbours(String word, HashSet<String> set){
+        List<String> neighbours = new ArrayList<>();
+        for (int i=0; i<word.length(); i++){
+            for (char ch='a'; ch<='z'; ch++){
+                if (ch== word.charAt(i)) continue;
+                String newWord = word.substring(0,i)+ ch + word.substring(i+1, word.length());
+                if (set.contains(newWord)) neighbours.add(newWord);
             }
-            steps++;
         }
-        return 0;
+        return neighbours;
+    }
+
+
+
+
+
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+         HashSet<String> set = new HashSet<>(wordList);
+         if (!set.contains(endWord)) return 0;
+         Queue <String> queue = new LinkedList<>();
+         queue.offer(beginWord);
+         if (set.contains(beginWord)){
+            set.remove(beginWord); 
+         }
+         int level =0;
+         while(!queue.isEmpty()){
+            int curLevelSize = queue.size();
+            for (int i= 0; i<curLevelSize; i++){
+                String node = queue.poll();
+                if (node.equals(endWord)){
+                    return level+1;
+                }
+            
+                 List<String> neighbours = getNeighbours(node, set);
+                for (String word: neighbours){
+                    if (set.contains(word)){
+                         queue.offer(word);
+                         set.remove(word);
+                    }
+
+                 }
+            }
+            level++;
+         } 
+         return 0;   
+        
+
     }
 }
